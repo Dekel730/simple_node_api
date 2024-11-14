0e7070676f10a202175d2931626ee5043f0416a5
Omer Gaiznger <Omerg863@gmail.com>
2024-11-14 12:19:41 +0200

    get comment

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createComment, getCommentByPost } from '../controllers/commentController.js';
+import { createComment, getCommentByPost, getComment } from '../controllers/commentController.js';
 
 const router = express.Router();
 
 router.route('/').post(createComment);
 router.route('/').get(getCommentByPost);
+router.route('/:id').get(getComment);
 
 export default router
